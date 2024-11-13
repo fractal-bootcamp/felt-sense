8a53519a682274f52da97658131fef219c697fc0
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-11-13 09:26:17 -0500

    move transcribe function to lib

--- src/app/api/stt/route.ts
+++ src/app/api/stt/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { transcribeFile } from "../../../lib/stt/transcribe";
 
 export async function POST(request: Request) {
   try {
@@ -28,25 +29,3 @@ export async function POST(request: Request) {
 if (!process.env.DEEPGRAM_API_KEY) {
   throw new Error("DEEPGRAM_API_KEY is not set");
 }
-
-export const transcribeFile = async (audioFile: Buffer): Promise<string> => {
-  console.log("here");
-  console.log("transcribing file");
-
-  const response = await fetch("https://api.deepgram.com/v1/listen", {
-    method: "POST",
-    headers: {
-      Authorization: `Token ${process.env.DEEPGRAM_API_KEY}`,
-      "Content-Type": "audio/wav",
-    },
-    body: audioFile, // Assuming audioFile is a Blob or a Buffer
-  });
-
-  if (!response.ok) {
-    throw new Error(`Deepgram API error: ${response.statusText}`);
-  }
-
-  const result = await response.json();
-  console.dir(result, { depth: null });
-  return result.results.channels[0].alternatives[0].transcript;
-};

f031e0973e260d4967ffac7b7122a5479ce4ed1f
mahlen <malink027@gmail.com>
2024-11-14 14:00:15 -0500

    getSession allows us to retrieve the user object on edge routes

--- src/app/api/stt/route.ts
+++ src/app/api/stt/route.ts
@@ -1,13 +1,18 @@
 import { NextResponse } from "next/server";
+import { getSession } from "@workos-inc/authkit-nextjs";
+import { transcribeFile } from "../../../lib/utils/transcribeFile";
 
 export async function POST(request: Request) {
   try {
+    const session = await getSession();
+    console.log('session', session);
     const audioBuffer = await request.arrayBuffer();
     const buffer = Buffer.from(audioBuffer);
 
     const result = await transcribeFile(buffer);
 
     return NextResponse.json({
+      user: session?.user,
       message: "Audio processed successfully",
       success: true,
       transcript: result,
@@ -28,25 +33,3 @@ export async function POST(request: Request) {
 if (!process.env.DEEPGRAM_API_KEY) {
   throw new Error("DEEPGRAM_API_KEY is not set");
 }
-
-export const transcribeFile = async (audioFile: Buffer): Promise<string> => {
-  console.log("here");
-  console.log("transcribing file");
-
-  const response = await fetch("https://api.deepgram.com/v1/listen", {
-    method: "POST",
-    headers: {
-      Authorization: `Token ${process.env.DEEPGRAM_API_KEY}`,
-      "Content-Type": "audio/wav",
-    },
-    body: audioFile, // Assuming audioFile is a Blob or a Buffer
-  });
-
-  if (!response.ok) {
-    throw new Error(`Deepgram API error: ${response.statusText}`);
-  }
-
-  const result = await response.json();
-  console.dir(result, { depth: null });
-  return result.results.channels[0].alternatives[0].transcript;
-};
--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,3 +1,4 @@
 import { handleAuth } from '@workos-inc/authkit-nextjs';
 
 export const GET = handleAuth({ returnPathname: '/' });
+console.log('auth callback', GET);
